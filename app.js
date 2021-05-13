d30d532513631ade76ffc22e2eefd5f13c1436ed
LSheNNawY <devshennawy@gmail.com>
2021-05-13 05:03:01 +0200

    fix deployment issue

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/api', express.static(path.join(__dirname, './public')));
 
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
     credentials: true
 }));
 
