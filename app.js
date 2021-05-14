d018cb1921509308fab52463a6ebdbf50d2741d1
LSheNNawY <devshennawy@gmail.com>
2021-05-14 10:28:53 +0200

    fix deployment issue

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/api', express.static(path.join(__dirname, './public')));
 
-app.set('trust proxy', 1)
+// app.set('trust proxy', 1)
 
 app.use(cors({
     origin: process.env.CLIENT_ORIGIN,
